72a91f51dcae8eb9fa81081bd22b1c7b6ce4d71b
Steven Demets <41109495+sdemets@users.noreply.github.com>
2022-12-02 13:48:16 +0100

    Add SSL support
    added three new parameters to the webhooks listener class to support SSL encryption.

--- webhook_listener/__init__.py
+++ webhook_listener/__init__.py
@@ -22,6 +22,9 @@ class Listener(object):
         self.logScreen = kwargs.get("logScreen", False)
         self.autoReload = kwargs.get("autoReload", False)
         self.handlers = kwargs.get("handlers", {})
+        self.sslModule = kwargs.get("sslModule", "builtin")
+        self.sslCert = kwargs.get("sslCert", "")
+        self.sslPrivKey = kwargs.get("sslPrivKey", "")
 
     def start(self):
         self.WEBTHREAD = threading.Thread(
@@ -42,6 +45,9 @@ class Listener(object):
                 "server.thread_pool": self.threadPool,
                 "engine.autoreload.on": self.autoReload,
                 "log.screen": self.logScreen,
+                "server.ssl_module": self.sslModule,
+                "server.ssl_certificate": self.sslCert,
+                "server.ssl_private_key": self.sslPrivKey,
             }
         }
         apiConf = {
