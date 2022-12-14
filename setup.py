71d62176ef0d354157f6b83ff788247a8ce5eb01
todd <toddrob@gmail.com>
2022-12-14 16:18:47 -0500

    refactor versioning to fix auto builds

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import setuptools
-from webhook_listener import version
+from distutils.util import convert_path
+
+# https://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
+main_ns = {}
+ver_path = convert_path("webhook_listener/version.py")
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="Webhook_Listener",
-    version=version.__version__,
+    version=main_ns["__version__"],
     author="Todd Roberts",
     author_email="todd@toddrob.com",
     description="Very basic webserver module to listen for webhooks and forward requests to predefined functions.",
@@ -14,7 +20,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/toddrob99/Webhooks",
     packages=setuptools.find_packages(),
-    install_requires=['cherrypy'],
+    install_requires=["cherrypy"],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
